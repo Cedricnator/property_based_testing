6edff0026404a4208143cb1384519bd6df237071
cedricnator <Cedric.kirmayr@gmail.com>
2025-10-03 18:28:01 -0300

    feat(database): create User model in Prisma schema and remove users table from init.sql

--- db/init.sql
+++ db/init.sql
@@ -6,13 +6,4 @@ ALTER DATABASE property_basedtesting SET timezone = 'America/Santiago';
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  email VARCHAR(255) NOT NULL UNIQUE,
-  first_name VARCHAR(150) NOT NULL,
-  last_name VARCHAR(150) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  is_active BOOLEAN NOT NULL DEFAULT TRUE,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
+
