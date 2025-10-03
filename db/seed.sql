3fc2d1a64ef594546ad6b257b863f959b660953d
cedricnator <Cedric.kirmayr@gmail.com>
2025-10-03 19:27:16 -0300

    feat(seed): add SQL seeding script and update package.json for execution

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+\c property_basedtesting;
+
+INSERT INTO users (id, email, first_name, last_name, password, is_active, created_at, updated_at) VALUES
+  ('d98a3462-d6ef-4227-9aac-f391b650c670', 'sofia.rojas@example.com', 'Sofía', 'Rojas', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('12568655-7a67-4357-a099-938994651654', 'mateo.gonzalez@example.com', 'Mateo', 'González', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('00649e08-cc04-4e91-b8b4-e18a269a2987', 'valentina.perez@example.com', 'Valentina', 'Pérez', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('b2cdd93e-c761-414c-9a0f-e9bb2f065fb8', 'benjamin.soto@example.com', 'Benjamín', 'Soto', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('1ed309e5-bcb3-4466-915f-afc83390b2bd', 'martina.fernandez@example.com', 'Martina', 'Fernández', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('07bf93e4-1aa6-403d-8e4e-354ce62dcd7e', 'lucas.vargas@example.com', 'Lucas', 'Vargas', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('47b341bf-747d-4f67-b15e-16099aef8189', 'antonia.sandoval@example.com', 'Antonia', 'Sandoval', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('09834738-52d0-4ce9-8517-2bfefb79a8bf', 'vicente.espinoza@example.com', 'Vicente', 'Espinoza', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('75283b57-a4ec-4741-93ee-d4d95a74bd0a', 'amelia.morales@example.com', 'Amelia', 'Morales', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW()),
+  ('70c82ca2-b751-4524-a2fb-19699d29ad2c', 'diego.carrasco@example.com', 'Diego', 'Carrasco', '$2b$10$abcdefghijklmnopqrstuv', true, NOW(), NOW());
